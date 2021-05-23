cc99617f8ccc76e2d2ec85d5a2e1669895eec87f
Daria Kurguzikova <daria.kurguzikova@intel.com>
2021-05-23 10:44:18 +0300

    add reg info

--- back.py
+++ back.py
@@ -1,18 +1,41 @@
 #Функция для получения информации по своту для каждой из категорий
 def get_swot_data():
+    #read Swot
     pass
     # return [{id:n0,name:n1,action:n2,importance:n3,probability:n4,power:n5},{}]
 
 #Функция обновления информации по своту
 def set_swot_data():
+    #read Swot
+    pass
+
+
+# read Пользователи
+def get_user_data(login):
+
+    pass
+
+
+#change Пользователи
+def set_user_data(login, password):
+
     pass
 
 #залогиниться
-def sign_in():
+def sign_in(login, password):
+    #read the login
+    #get_user_data(login)
+    #check if the person exist
+    #if so read get_user_data(login)
+    #check password
     pass
 
 #зарегаться
-def sign_up():
+def sign_up(login, password):
+    #read the login
+    #get_user_data(login)
+    #check if the person exist
+    #if not set_user_data(login)
     pass
 
 #функция для построения графиков
