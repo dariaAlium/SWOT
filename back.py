f23896a5e17efc232d70312f43999939aae4f366
Daria Kurguzikova <daria.kurguzikova@intel.com>
2021-05-23 10:29:51 +0300

    Init commit

---
+++ back.py
@@ -0,0 +1,20 @@
+#Функция для получения информации по своту для каждой из категорий
+def get_swot_data():
+    pass
+    # return [{id:n0,name:n1,action:n2,importance:n3,probability:n4,power:n5},{}]
+
+#Функция обновления информации по своту
+def set_swot_data():
+    pass
+
+#залогиниться
+def sign_in():
+    pass
+
+#зарегаться
+def sign_up():
+    pass
+
+#функция для построения графиков
+def get_plot():
+    pass
