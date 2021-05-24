7023c009ca4395ae592e06e44445f9b9a349989d
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2021-05-24 15:55:59 +0300

    создание свот таблицы

--- back.py
+++ back.py
@@ -1,32 +1,44 @@
 import sqlite3
 from sqlite3 import Error
 sqlite_connection= None; #подклюение к бд
+cursor=None; #курсор для работы с бд
 bd="swot.db" #файл с бд
 current_user=""; #текущий логин пользователя
+current_project="";
 #создаем БД со всеми таблицами, вызывается единожды
 
 
 
+def create_swot_table():
+    cursor.execute("CREATE TABLE IF NOT EXISTS Swot(line integer, project integer,type text,"
+                   "name text, action text, importance text, probability text,FOREIGN KEY (project) REFERENCES "
+                   "projects (id))")
+
 
 
 #Функция для получения информации по своту для каждой из категорий
-def get_swot_data():
+def get_swot_data(type):
     #read Swot
     pass
     # return [{id:n0,name:n1,action:n2,importance:n3,probability:n4,power:n5},{}]
 
 #Функция обновления информации по своту
-def set_swot_data():
+def set_swot_data(line,type,name,action,importance,probabilty):
+
+
     #read Swot
     pass
 
+def change_project(new_project):
+    pass
 
+def create_new_project(new_project):
+    pass
 # read Users
 def get_user_data(cursor, login):
     sqlite_read_query = """select * from Users where login =? """
     cursor.execute(sqlite_read_query, (login,))
     return cursor.fetchall()
-
 
 #change Пользователи
 def set_user_data(login, password):
@@ -57,7 +69,7 @@ def sign_up(cursor, login, password):
 
 
 #функция для построения графиков
-def get_plot():
+def get_plot(type):
     pass
 
 try:
@@ -69,11 +81,14 @@ try:
     cursor.execute(sqlite_select_query)
     record = cursor.fetchall()
     print("Версия базы данных SQLite: ", record)
-    cursor.close()
+
 except sqlite3.Error as error:
     print("Ошибка при подключении к sqlite", error)
+''''
 finally:
     if (sqlite_connection):
+        cursor.close()
         sqlite_connection.close()
         print("Соединение с SQLite закрыто")
-
+'''
+create_swot_table()
