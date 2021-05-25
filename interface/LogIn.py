c3a7f2cb17719ff456d8f03732aaeca64969b603
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2021-05-25 14:28:42 +0300

    Main menu

---
+++ interface/LogIn.py
@@ -0,0 +1,21 @@
+from PyQt5 import uic
+from PyQt5 import QtWidgets
+import sys
+
+from PyQt5.QtWidgets import QMainWindow
+
+
+class Ui_LogIn(QMainWindow):
+   def __init__(self):
+      super(QMainWindow, self).__init__()
+      uic.loadUi('logIn.ui', self)
+      self.setFixedSize(500, 500)
+      self.show()
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = Ui_LogIn()
+    myapp.show()
+    sys.exit(app.exec_())
