030a46c616ddf93c90f37bb977205b361fd4290f
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2021-05-25 16:20:30 +0300

    Main menu

--- interface/MainMenu.py
+++ interface/MainMenu.py
@@ -10,22 +10,33 @@ class Ui_Main(QMainWindow):
         uic.loadUi('mainmenu.ui', self)
         #self.setFixedSize(500, 500)
         self.setFixedSize(900, 700)
-        self.pushButton.clicked.connect(self.set_weakness)
+        self.get_weakness()
+        try:
+            self.pushButton.clicked.connect(self.set_weakness)
+        except Exception as e:
+            print(e)
+
+
         self.show()
     def set_weakness(self):
-        names=[self.lineEdit_2,self.lineEdit_4,self.lineEdit_6,self.lineEdit_8,self.lineEdit_10]
-        actions=[self.lineEdit_3,self.lineEdit_5,self.lineEdit_7,self.lineEdit_9,self.lineEdit_11]
-        importance=[self.spinBox,self.spinBox_3,self.spinBox_5,self.spinBox_7,self.spinBox_9]
-        probability=[self.spinBox_2,self.spinBox_4,self.spinBox_6,self.spinBox_8,self.spinBox_10]
-        power=[self.label_22,self.label_23,self.label_24,self.label_25,self.label_26]
-        for i in range(0,6):
-            name=names[i].text()
-            if(name):
-                action=actions[i].text()
-                imp=importance[i].text()
-                prob=probability[i].text()
-                set_swot_data(i,"weaknesses",action,imp,prob)
-        #вызвать функцию гет
+        try:
+            names=[self.lineEdit_2,self.lineEdit_4,self.lineEdit_6,self.lineEdit_8,self.lineEdit_10]
+            actions=[self.lineEdit_3,self.lineEdit_5,self.lineEdit_7,self.lineEdit_9,self.lineEdit_11]
+            importance=[self.spinBox,self.spinBox_3,self.spinBox_5,self.spinBox_7,self.spinBox_9]
+            probability=[self.spinBox_2,self.spinBox_4,self.spinBox_6,self.spinBox_8,self.spinBox_10]
+            power=[self.label_22,self.label_23,self.label_24,self.label_25,self.label_26]
+            for i in range(0,5):
+                print(i)
+                name=names[i].text()
+                if(name):
+                    action=actions[i].text()
+                    imp=importance[i].text()
+                    prob=probability[i].text()
+                    set_swot_data(i,"weaknesses",name,action,imp,prob)
+
+            self.get_weakness()
+        except Exception as e:
+            print(e)
 
     def get_weakness(self):
         names = [self.lineEdit_2, self.lineEdit_4, self.lineEdit_6, self.lineEdit_8, self.lineEdit_10]
@@ -33,16 +44,15 @@ class Ui_Main(QMainWindow):
         importance = [self.spinBox, self.spinBox_3, self.spinBox_5, self.spinBox_7, self.spinBox_9]
         probability = [self.spinBox_2, self.spinBox_4, self.spinBox_6, self.spinBox_8, self.spinBox_10]
         power = [self.label_22, self.label_23, self.label_24, self.label_25, self.label_26]
-        res=get_swot_data()
+        res=get_swot_data("weaknesses")
         for r in res:
-            names[r["id"]].setText()
+            i=r["id"]
+            names[i].setText(r["name"])
+            actions[i].setText(r["action"])
+            importance[i].setValue(r["importance"])
+            probability[i].setValue(r["probability"])
+            power[i].setText(str(r["power"]))
 
-            name = names[i].text()
-            if (name):
-                action = actions[i].text()
-                imp = importance[i].text()
-                prob = probability[i].text()
-                get_swot_data("weaknesses")
 
 
 
