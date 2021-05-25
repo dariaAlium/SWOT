6afb6021dd59e5e80e3491c9f9549321f43c7ddc
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2021-05-25 15:56:32 +0300

    Main menu

--- interface/MainMenu.py
+++ interface/MainMenu.py
@@ -1,10 +1,51 @@
-class Ui_LogIn(QMainWindow):
+from PyQt5 import uic
+from PyQt5 import QtWidgets
+import sys
+from PyQt5.QtWidgets import QMainWindow
+from back import *
+
+class Ui_Main(QMainWindow):
     def __init__(self):
         super(QMainWindow, self).__init__()
-        uic.loadUi('logIn.ui', self)
-        self.setFixedSize(500, 500)
-        self.logInButton.clicked.connect(self.logInUI)
-        # self.show()
+        uic.loadUi('mainmenu.ui', self)
+        #self.setFixedSize(500, 500)
+        self.setFixedSize(900, 700)
+        self.pushButton.clicked.connect(self.set_weakness)
+        self.show()
+    def set_weakness(self):
+        names=[self.lineEdit_2,self.lineEdit_4,self.lineEdit_6,self.lineEdit_8,self.lineEdit_10]
+        actions=[self.lineEdit_3,self.lineEdit_5,self.lineEdit_7,self.lineEdit_9,self.lineEdit_11]
+        importance=[self.spinBox,self.spinBox_3,self.spinBox_5,self.spinBox_7,self.spinBox_9]
+        probability=[self.spinBox_2,self.spinBox_4,self.spinBox_6,self.spinBox_8,self.spinBox_10]
+        power=[self.label_22,self.label_23,self.label_24,self.label_25,self.label_26]
+        for i in range(0,6):
+            name=names[i].text()
+            if(name):
+                action=actions[i].text()
+                imp=importance[i].text()
+                prob=probability[i].text()
+                set_swot_data(i,"weaknesses",action,imp,prob)
+        #вызвать функцию гет
+
+    def get_weakness(self):
+        names = [self.lineEdit_2, self.lineEdit_4, self.lineEdit_6, self.lineEdit_8, self.lineEdit_10]
+        actions = [self.lineEdit_3, self.lineEdit_5, self.lineEdit_7, self.lineEdit_9, self.lineEdit_11]
+        importance = [self.spinBox, self.spinBox_3, self.spinBox_5, self.spinBox_7, self.spinBox_9]
+        probability = [self.spinBox_2, self.spinBox_4, self.spinBox_6, self.spinBox_8, self.spinBox_10]
+        power = [self.label_22, self.label_23, self.label_24, self.label_25, self.label_26]
+        res=get_swot_data()
+        for r in res:
+            names[r["id"]].setText()
+
+            name = names[i].text()
+            if (name):
+                action = actions[i].text()
+                imp = importance[i].text()
+                prob = probability[i].text()
+                get_swot_data("weaknesses")
+
+
+
 
     def logInUI(self):
         login = self.loginLine.text()
@@ -16,3 +57,8 @@ class Ui_LogIn(QMainWindow):
         # self.Open = SecretQuestionWin(user)
         # self.Open.show()
         print("hi")
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = Ui_Main()
+    myapp.show()
+    sys.exit(app.exec_())
